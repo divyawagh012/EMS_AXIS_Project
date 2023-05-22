4dd6208bc78dc05e68e806b33d561fe7760b03ff
divyawagh012 <divyawagh012@gmail.com>
2023-05-22 17:05:11 +0530

    Ems Project

---
+++ src/main/java/com/example/demoprojectevent/DemoprojecteventApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoprojectevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojecteventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoprojecteventApplication.class, args);
+        System.out.println("EMS Demo");
+    }
+
+}
---
+++ src/test/java/com/example/demoprojectevent/DemoprojecteventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoprojectevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojecteventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
